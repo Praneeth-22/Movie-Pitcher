85be5d51f2f284e720867ace1b8f7b3613a9968b
Praneeth22 <yennampraneeth@gmail.com>
2024-01-08 12:38:17 -0500

    feat:ui styling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>hi</p>
+      <div id="layout">
+        <div id="header">
+          <h3>MoviePitch</h3>
+        </div>
+        <div id="body">
+          <div id="body_content">
+            <div id="body_content_info">
+              <p>left</p>
+              <p>right</p>
+            </div>
+            <div id="body_content_interact">
+              <p>serach</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
